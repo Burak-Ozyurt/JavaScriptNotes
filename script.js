c8711e5ae6c8ac0ccc329e979edb9d9051b04573
Burak <Burak_ozyurt42@outlook.com>
2026-04-17 19:38:48 +0300

    while, do-while, for döngüsü ve dizi ve objelerde kullanımı

--- script.js
+++ script.js
@@ -1,147 +1,149 @@
-// Koşullu Durumlar (If-Else)
 
-const firstName = "Burak";
-const userName = "burakozyurt";
-const age = 21;
-const isStudent = true;
-const school = "university";
+// Döngüler (Loops)
 
-if (userName == "burakozyurt") {
-    console.log("Merhaba Burak");
-}
-else {
-    console.log("Kullanıcı bulunamadı!");
-}
 
-// if (age === 21)    // 3 eşittir hem veriyi hem de tipi sorgular
+// while
+
+// let i = 0;
+// while (i < 10)   // sonsuz döngü olur
 // {
-//     console.log("Yaşınız: 21");
+//     console.log(i);
+//     i++; // i+=1
 // }
 
-if (isStudent)  // (isStudent === true aynı)
-{
-    console.log("Hangi bölümde okuyorsun?");
-}
-else {
-    console.log("Hangi mesleği yapıyorsunuz?");
-}
-
-if (age >= 18) {
-    if (school == "university") {
-        console.log("Ehliyet alabilirsin");
-    }
-    else {
-        console.log("Eğitim durumun ehliyet alabilmek için yeterli değil!");
-    }
-}
-else {
-    console.log("Ehliyet alacak yaşa daha gelmedin!");
-}
-
-let id = "23132";
-if (typeof id != "undefined") {
-    console.log("id: " + id);
-}
-else {
-    console.log("no id!");
-}
-
-
-// Switch Koşul Yapısı
-
-// let islem = 1;
-
-// switch (islem) {
-//     case 1:
-//         console.log("1 no'lu işlem yapıldı");
-//         break;
-//     case 2:
-//         console.log("2 no'lu işlem yapıldı");
-//         break;
-//     case 3:
-//         console.log("3 no'lu işlem yapıldı");
-//         break;
-//     default:
-//         console.log("İşlem yapılmadı!");
-// }
-
-
-// let day;
-
-// switch (new Date().getDay())
+// let i = 10;
+// while (i > 0)
 // {
-//     case 0:
-//         console.log("Pazar");
-//         break;
-//     case 1:
-//         console.log("Pazartesi");
-//         break;
-//     case 2:
-//         console.log("Salı");
-//         break;
-//     case 3:
-//         console.log("Çarşamba");
-//         break;
-//     case 4:
-//         console.log("Perşembe");
-//         break;
-//     case 5:
-//         console.log("Cuma");
-//         break;
-//     case 6:
-//         console.log("Cumartesi");
-//         break;
+//     console.log(i);
+//     i--;
 // }
 
-// console.log(day);
+// break ve continue
+
+// let i = 0;
+// while (i < 10)
+// {
+//     console.log(i);
+//     if (i == 6)
+//     {
+//         break;
+//     }
+//     i++;
+// }
+
+// let i = 0;
+// while (i < 10)
+// {
+//     if (i == 6)
+//     {
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// }
 
 
-let hour = 16;
+// do while
 
-switch (true)
+// let i = 0;
+// do
+// {
+//     console.log(i);
+//     i++;
+// }
+// while(i < 10);
+
+
+// for
+
+// for (let i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+
+// for (let i=0;i<10;i++)
+// {
+//     if (i==3)
+//     {
+//         console.log("Seçilen rakam:" + i);
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let toplam = 0;
+
+// for (let i=1;i<10;i++)
+// {
+//     toplam+=i;
+// }
+// console.log("Toplam: " + toplam);
+
+// let sonuc = 1;
+
+// for (let i=1;i<6;i++)
+// {
+//     sonuc*=i;
+// }
+// console.log("Sonuç: " + sonuc)
+
+
+
+// Dizi ve Objelerde Döngüler
+
+
+let cities = ["İstanbul","Konya","Ankara","Sakarya","İzmir"];
+
+let users = 
+[
+    {firstName: "Burak",lastName:"Özyurt"},
+    {firstName: "Nisa",lastName:"Koçak"},
+    {firstName: "Hasan",lastName:"Demir"},
+    {firstName: "Utku",lastName:"Çal"},
+    {firstName: "Ahmet",lastName:"Yiğit"}
+]
+
+// Diziler
+
+// for(let i=0;i<cities.length;i++)
+// {
+//     console.log(cities[i]);
+// }
+
+
+// for-in metodu
+
+// for (let i in cities)
+// {
+//     console.log(`index: ${i} value: ${cities[i]}`);
+// }
+
+// cities.forEach(function(item){
+//     console.log(item);
+// });
+
+// for (let i=0;i<users.length;i++)
+// {
+//     console.log(users[i].firstName);
+// }
+
+// for (let i in users)
+// {
+//     console.log(`index: ${i} value: ${users[i].firstName}`);
+// }
+
+
+// map metodu (dizilerde)
+
+// let veri = users.map(function(item)
+// {
+//     return item.firstName + " " + item.lastName;
+// });
+// console.log(veri);
+
+let numbers = [1,3,5,6,8,9,12,43];
+let num = numbers.map(function(n)
 {
-    case (hour >= 6 && hour <= 12):
-        console.log("Günaydın!");
-        break;
-    case (hour >= 13 && hour <= 18):
-        console.log("İyi Günler!");
-        break;
-    case (hour >= 19 && hour <= 24):
-        console.log("İyi Akşamlar!");
-        break;
-    default:
-        console.log("Yanlış Zaman!");
-}
-
-
-// Object Literals
-
-// let name = "Burak";
-// let lastName = "Özyurt";
-                       // Bu yöntemlerle birden fazla oldu mu karışık oluyor
-// let burak = ["Burak","Özyurt",21,];
-// let nisa = ["Nisa","Koçak",20];
-
-let veri;
-let person = {
-    personName : "Burakozyrt",
-    personFirstName : "Burak",
-    personLastName : "Özyurt",
-    personAge : 21,
-    hobbies : ["espor","yazılım",],
-    address : {
-        city : "Konya",
-        country : "Türkiye",
-        phone : "0500000000",
-    }
-}
-
-veri = person;
-veri = person.address;
-veri = person.personFirstName;
-veri = person.hobbies;
-veri = person.address.city;
-veri = person.address.phone;
-
-console.log(veri);
-console.log(typeof person);
+    return n*n;
+});
+console.log(num);
