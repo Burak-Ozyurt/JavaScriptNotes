3871181bbb6b997071dcb54041713b591917b1ca
Burak <Burak_ozyurt42@outlook.com>
2026-04-20 11:43:07 +0300

    tekli element seçimi getelementbyid ve queryselector metodu

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
+
+// Elementlerin Seçilmesi
+
+// -- Tek Element Seçimi --
+
+// document.getElementById() metodu
+
 let veri;
 
-veri = window.document;
-veri = window.document.head;
-veri = window.document.body;
-veri = window.document.URL;
-veri = window.document.domain;
-veri = window.document.images;
-veri = window.document.forms;
-veri = window.document.forms[0].id;
-veri = window.document.forms[0].method;
-veri = window.document.forms[0].action;
-veri = window.document.scripts;
-veri = window.document.scripts[2];
+// veri = document.getElementById("header");
+// veri = document.getElementById("header").id;
+// veri = document.getElementById("header").className;
 
-console.log(veri);
+// veri = document.getElementById("header");  
+//veri = veri.id;         // uzun uzun yapmak yerine böyle de olur
+//veri = veri.className;
+
+// veri.style.color = "blue";
+// veri.style.fontSize = "50px";
+//veri.style.fontWeight = "bold";
+//veri.style.display = "none";
+
+// document.getElementById("header").innerText = "Yapılacaklar";
+// document.getElementById("header").innerText = "<b> Yapılacaklar </b>";  // burda ekranda <b> kısımları da yazıyor. Ondan dolayı innerText yerine innerHTML
+// document.getElementById("header").innerHTML = "<b> Yapılacaklar </b>";
+
+// console.log(veri);
+
+
+// document.querySelector() metodu
+
+//console.log(document.querySelector("#header"));
+//console.log(document.querySelector(".card-header"));
+//console.log(document.querySelector("div"));
+
+// document.querySelector("li").style.color = "blue";  // ilk elemanı yaptı
+// document.querySelector("li:last-child").style.color = "orange";   // son elemanı
+// document.querySelector("li:nth-child(2)").style.color = "brown";
+
+document.querySelector("li").className = "list-group-item list-group-item-danger";   // class değişimi
+
+document.querySelector("li").classList.add("active");
